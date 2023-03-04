0dc0d394c322000b3fa90e340ee4c98137557cfc
pratik <pratikdholariya97255@gmail.com>
2023-03-05 01:25:15 +0530

    adding some extra image for good looking website and add some product

--- shop/views.py
+++ shop/views.py
@@ -8,11 +8,20 @@ def index(request):
     products = Product.objects.all()
     n = len(products)
     nslides = n//4 + ceil(n/4 - n//4)
+    allProds = []
+    catProds = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catProds}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nslides = n // 4 + ceil(n / 4 - n // 4)
+        print(prod)
+        allProds.append([prod, range(n, nslides), nslides])
     # params = {'slides': nslides, 'range': range(1, nslides), 'product': products}
-    allProds = [[products, range(1, nslides), nslides],
-                [products, range(1, nslides), nslides]]
+    # allProds = [[products, range(1, nslides), nslides],
+    #             [products, range(1, nslides), nslides]]
     params = {'allprods': allProds}
-    return render(request, 'shop/index.html', params)
+    return render(request, 'shop/temp.html', params)
 
 
 def about(request):
